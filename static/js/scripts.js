25a74503f2a50d31768bdc14a5a4d7ca1ec17794
Spenser <spenser@172-29-240-150.dynapool.nyu.edu>
2019-10-22 21:04:04 -0400

    added forms

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,36 +1,40 @@
 'use scrict';
-
 function clickHandlers() {
-  console.log(event.target);
   if (event.target.matches('#pull')) {
-    document.querySelector('body').classList.toggle('show-nav');
+    showMenu();
     event.preventDefault();
   }
-  // event.preventDefault();
+  if (event.target.matches('.content-video a')) {
+    videoSwitch();
+    event.preventDefault();
+  }
+  if (event.target.matches('.image-tn img')) {
+    runCarousel();
+    event.preventDefault();
+  }
 }
-//video
-// const videoLinks = document.querySelectorAll('.content-video a');
-// const videoLinksArray = [...videoLinks];
-const iFrame = document.querySelector('iframe');
-const videoLinks = document.querySelectorAll('.content-video a');
-videoLinks.forEach(videoLink =>
-  videoLink.addEventListener('click', selectVideo),
-);
 
-function selectVideo() {
-  removeActiveClass(); // NEW
-  this.classList.add('active'); // NEW
+function runCarousel() {
+  const imageHref = event.target.parentNode.getAttribute('href');
+  const titleText = event.target.title;
+  document.querySelector('figure img').setAttribute('src', imageHref);
+  document.querySelector('figcaption').innerHTML = titleText;
+}
+
+var showMenu = function() {
+  document.querySelector('body').classList.toggle('show-nav');
+};
+
+var videoSwitch = function() {
+  const iFrame = document.querySelector('iframe');
+  const videoLinks = document.querySelectorAll('.content-video a');
+  videoLinks.forEach(videoLink => videoLink.classList.remove('active'));
+  event.target.classList.add('active');
   const videoToPlay = event.target.getAttribute('href');
   iFrame.setAttribute('src', videoToPlay);
-  event.preventDefault();
-}
+};
 
-// NEW
-function removeActiveClass() {
-  videoLinks.forEach(videoLink => videoLink.classList.remove('active'));
-}
-//end video
-
+//NYT
 var addContent = function(data) {
   var looped = '';
   for (let i = 0; i < data.results.length; i++) {
